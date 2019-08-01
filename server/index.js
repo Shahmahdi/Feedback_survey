a37f873bc7fe7818f392a922efdb4f5fd5f02dab
mahdi <shahmahdi11@gmail.com>
2019-08-02 03:24:07 +0600

    initial commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('First route');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`server is running..`);
+});
